adb9437eba1be7ab27a77a9103c2fe9c6514af7d
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-01-17 10:17:47 +0100

    v2

--- SkysFormsDemo/Pages/Person/Index.cshtml.cs
+++ SkysFormsDemo/Pages/Person/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SkysFormsDemo.Data;
 using SkysFormsDemo.Services;
 
 namespace SkysFormsDemo.Pages.Person
@@ -8,6 +10,7 @@ namespace SkysFormsDemo.Pages.Person
     public class IndexModel : PageModel
     {
         private readonly IPersonService _personService;
+        private readonly ApplicationDbContext _context;
         public List<PersonViewModel> Persons { get; set; }
 
         public class PersonViewModel
@@ -18,9 +21,19 @@ namespace SkysFormsDemo.Pages.Person
             public string Email { get; set; }
         }
 
-        public IndexModel(IPersonService personService)
+        public IndexModel(IPersonService personService, ApplicationDbContext context)
         {
             _personService = personService;
+            _context = context;
+        }
+
+        public IActionResult OnGetFetchInfo(int id)
+        {
+            var person = _context.Person.Include(e => e.OwnedCars).First(e => e.Id == id);
+            return new JsonResult(new { 
+                namn = person.Name, 
+                antalBilar = person.OwnedCars.Count });
+
         }
 
         public void OnGet()
